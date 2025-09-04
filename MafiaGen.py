41a858f5208ab0a6092d7ac28401319129ba0b61
SLYR <peteraltforstuff@gmail.com>
2025-09-04 16:53:14 -0400

    Made GM Panel interactive! Also added to-do list.

--- MafiaGen.py
+++ MafiaGen.py
@@ -1,7 +1,16 @@
 import random
 import os
 
-#GLOBAL
+#To Do:
+#Impliment twin system
+#GM control screen
+#Reprint player list if confusion
+#
+#
+#
+# 
+# 
+# GLOBAL
 Maf_Ratio = 4
 
 def Clr_terminal():
@@ -73,19 +82,38 @@ def GmSheet():
         print(f"{player} is {role}")
 
 print("\nAll roles assigned!\n")
-for player, role in player_roles.items():
-    Clr_terminal()
-    print(f"{player} is... (press any key to reveal)")
-    os.system('pause')
-    print(role)
-    os.system('pause')
-    Clr_terminal()
+def Reveal():
+    for player, role in player_roles.items():
+        Clr_terminal()
+        print(f"{player} is... (press any key to reveal)")
+        os.system('pause')
+        print(role)
+        os.system('pause')
+        Clr_terminal()
 
 print("Good luck, and have fun!")
 
 os.system('pause')
 Clr_terminal()
 
-print("Welcome GM! Here is the roles sheet!\n")
-GmSheet()
 
+while True:
+    try:
+        choice = input("Welcome GM! Here is your operation panel!\n\nTo print the whole player list Enter 1\nTo Reveal all players Enter 2\nTo quit the program enter QUIT\n\nEnter command:  ")
+        if choice == "1":
+            print("\n")
+            GmSheet()
+            os.system('pause')
+            Clr_terminal()
+        if choice == "2":
+            print("\n")
+            Reveal()
+            Clr_terminal()
+        if choice == "QUIT":
+            Clr_terminal()
+            print("Game Concluded, Thanks for playing!")
+            os.system('pause')
+            break
+    except ValueError:
+        Clr_terminal()
+        print("Error! Invalid Value!")
